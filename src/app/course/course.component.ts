a36935aa2a61ac2ef91ddf94371bba37028505b9
bbmattieu9 <bbmattieu9@gmail.com>
2025-03-11 23:57:25 +0100

    [Fix] remove unused imports in course component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,12 +1,12 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import { MatTableDataSource } from "@angular/material/table";
+// import { MatPaginator } from "@angular/material/paginator";
+// import { MatSort } from "@angular/material/sort";
+// import { MatTableDataSource } from "@angular/material/table";
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
 import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from "rxjs";
+// import {merge, fromEvent} from "rxjs";
 
 
 @Component({
